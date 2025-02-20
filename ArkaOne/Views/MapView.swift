04b79c80562bc45b1d0fb2220ccf5128dbf9b6d8
tandyys <tansabana12@gmail.com>
2025-02-20 17:21:12 +0700

    Working on MapKit and CoreLocation

--- ArkaOne/Views/MapView.swift
+++ ArkaOne/Views/MapView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @StateObject private var locationManager = LocationManager()
-    
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
+    @State var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.785834, longitude: -122.406417), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+
     
     var body: some View {
         VStack {
-            if let region = locationManager.region {
-                Map(coordinateRegion: $locationManager.region, showsUserLocation: true)
+            Map(coordinateRegion: $mapRegion, showsUserLocation: true)
+            
+//            Map(initialPosition: position, interactionModes: [.zoom])
+//                .onMapCameraChange { context in
+//                    print(context.region)
+//                }
+            
+            HStack(spacing: 50) {
+                Button("Paris") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+
+                Button("Tokyo") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
             }
         }
     }
